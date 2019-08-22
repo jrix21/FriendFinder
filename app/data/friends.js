a2a64a537eddebe0191a32ff211e1e6347aba791
jrix21 <jrix21@gmail.com>
2019-08-22 15:47:51 -0500

    initial HTML Set

---
+++ app/data/friends.js
@@ -0,0 +1,8 @@
+//Here is where we will store the friends array
+
+// Name
+// Photo
+// Scores (Separate Array)
+
+// will  need to export this data to based on users answers to questions
+// closest score to user  will be selected and sent to friends.html
---
+++ app/data/survey.js
@@ -0,0 +1,3 @@
+// here we are going to push the data from the form submission
+// name, photo url, quest1 thru quest10 should be stored into the friendArray
+// then the scores from each question is totalled to find the closest friend near the total
